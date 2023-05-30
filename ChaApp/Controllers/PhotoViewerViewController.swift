226547b4c1313e7d7877d771c8fb4605595c2955
Ayoub Zouine <100783340+ayoubzouine94@users.noreply.github.com>
2023-05-30 12:53:05 +0100

    Wrap and getting started

---
+++ ChaApp/Controllers/PhotoViewerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoViewerViewController.swift
+//  ChaApp
+//
+//  Created by Tami Zouine on 30/5/2023.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
